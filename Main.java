9f4722ff3d7d6d4393363a990ca0dee6feb069b7
werwik640 <gribanof2020@mail.ru>
2025-01-20 15:48:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        Car car1 = new Car("LADA", "Granta", 2011);
-        Car car2 = new Car("Mitsubishi", "Eclipse",1995);
-        Car car3 = new Car("Mitsubishi", "Materials", 2021);
+        Car car1 = new Car("Ford", "Focus", 2008);
+        Car car2 = new Car("Toyota", "Corolla",2001);
+        Car car3 = new Car("Hyundai", "Solaris", 2012);
         car1.drive(50);
         car2.drive(2010);
         System.out.println(car1.getFuelLevel());
